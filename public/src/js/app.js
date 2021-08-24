70efa42858023dbe81ae272727fada7eccc81494
Vanesa Burman <vburmi@hotmail.com>
2021-08-24 09:34:39 -0300

    Add files finished

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,9 +1,10 @@
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    //.register('/sw.js', {scope: '/help/'}) HTTPS: requested for pwa sw
     .register('/sw.js')
     .then(function() {
       console.log('Service worker registered!');
+      console.log(navigator)
+      console.log(navigator.serviceWorker)
     });
 }
