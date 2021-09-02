9369d756aa215657cae2fca660250c45fb44a04d
Vanesa Burman <vburmi@hotmail.com>
2021-09-02 16:14:46 -0300

    Agregando Caché Static / Dinamic

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,10 +1,69 @@
 
+//chequear si el browser soporta promesas o no:
+if(!window.Promise){
+  window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
     .then(function() {
       console.log('Service worker registered!');
-      console.log(navigator)
-      console.log(navigator.serviceWorker)
     });
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+
+  e.preventDefault();
+  deferredPrompt = e;
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice
+    .then((choiceResult)=> {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      }
+    deferredPrompt = null;
+    });
+});
+
+// window.onload = (e) => { 
+//   let deferredPrompt;
+//   window.addEventListener('beforeinstallprompt', (e) => {
+//     // Prevent Chrome 67 and earlier from automatically showing the prompt
+    
+//     console.log('beforeinstallprompt fired');
+//     e.preventDefault();
+//     // Stash the event so it can be triggered later.
+//     deferredPrompt = e;
+//   });
+//   // Show the prompt
+//   deferredPrompt.prompt();
+// }
+
+
+fetch('https://httpbin.org/ip')
+  .then(response => {
+      console.log('Response con FETCH...');
+      console.log(response);
+      return response.json();
+  })
+  .then(data => console.log(data))
+  .catch(error => {
+      console.log('Error on fetch call...', error);
+  });
+
+  fetch('https://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+      'Content-Type':'application/json',
+      'Accept': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify({message: 'Este es el body'})
+  })
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+   
--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,56 @@
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static-v3')
+      .then(cache => {
+        cache.add('/')
+        cache.addAll([
+          '/index.html',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          '/src/css/feed.css'
+        ])
+      }
+
+      )
+  )
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', event => {
   console.log('[Service Worker] Activating Service Worker ...', event);
+
+  event.waitUntil(
+    caches.keys()
+      .then(ListKey => {
+        return Promise.all(ListKey.map(Key => {
+      if(Key != 'static' && Key != 'dinamic') {
+        console.log('[SW] removing old cache', Key)
+        return caches.delete(Key)
+        }
+      })
+      )
+    }
+    )
+  )
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ....', event);
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response
+        } else {
+          return fetch(event.request)
+            .then(res=> {
+              return caches.open('dinamic')
+              .then(cache=>{
+                cache.put(event.request.url, res.clone());
+                return res;
+              })
+            })
+        }
+      })
+  )
 });
